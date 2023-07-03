823cb142ce3ac025b8071640ac096fa080742444
kozhukovv <kozhukovv@yandex.ru>
2023-07-03 21:24:06 +0300

    deleting token

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pd.set_option('display.max_columns', None)
 pd.options.display.expand_frame_repr = False
 pd.options.display.max_colwidth = False
 
-#KEY = '4b6871c642e0c92c6e34132c53aae2a2' твой ключ
+#KEY = 'xxxxxxxxxx' #твой ключ
 
 scopus = Scopus(KEY)
 
